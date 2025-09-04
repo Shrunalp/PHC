6c307c5e2f6ee66f8fb877248e7ff169f5098cb5
Shrunalp <60906467+Shrunalp@users.noreply.github.com>
2025-09-04 12:41:56 -0400

    Add files via upload

--- PHC/__init__.py
+++ PHC/__init__.py
@@ -1,6 +1,6 @@
 from .image_conditioning import preprocess
 from .local_ph import PHC
 from .filtrations import ext_persistence, alphacomplex, cubicalcomplex
-from .utils import noise_pts, pointcloud2D
+from .utils import pointcloud2D, remove_noisy_pts
 
-__all__ = ['PHC', 'preprocess', 'ext_persistence', 'alphacomplex', 'cubicalcomplex', 'noise_pts', 'pointcloud2D']
+__all__ = ['PHC', 'preprocess', 'ext_persistence', 'alphacomplex', 'cubicalcomplex', 'remove_noisy_pts', 'pointcloud2D']
