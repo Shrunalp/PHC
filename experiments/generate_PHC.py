57acab5d0ff156477c4c00b8ed92ea0fd1bc6672
Shrunalp <60906467+Shrunalp@users.noreply.github.com>
2026-03-02 00:03:34 -0500

    Add files via upload

--- experiments/generate_PHC.py
+++ experiments/generate_PHC.py
@@ -2,21 +2,33 @@ from PHC import PHC, preprocess
 import numpy as np
 from tqdm import tqdm
 
+#Generate various PHC datasets with custom parameters
+classes = ["alpha", "lower_star", "ext_adj_complex"]
+thresh_param = [195, 200]
+dimension = [0,1]
+
 histo_imgs = np.load("grey_ost_img/balanced_img.npy", allow_pickle=True)
 lb = np.load("grey_ost_img/balanced_lb.npy", allow_pickle=True)
-conditioning = preprocess(thresh=200, iterate=1)
-PHC_data = np.empty((1143,256,20,20)) 
+for type in classes:
+    for param in thresh_param:
+        for dim in dimension:
+            data_type = "Computing PHC - "+type+" t"+str(param)+" dim "+str(dim)
+            conditioning = preprocess(thresh=param, iterate=1)
+            PHC_data = np.empty((1143,20,20)) 
+            for img in tqdm(range(len(histo_imgs)), mininterval=5.0, desc=data_type):
+                thresh_img = conditioning.threshold(histo_imgs[img])
+                prepped_image = conditioning.dilate(thresh_img) 
+                localhom = PHC(persistence_type=type, stride=512, window_size=512, dimension=dim) 
+                data = localhom.convolve(prepped_image)
+                data = np.array(data).reshape((20,20))
+                PHC_data[img] = data
+            
+            print("Saving File")
+            np.save(str(dim)+"_"+type+"_PHC_data_t"+str(param), PHC_data, allow_pickle=True)
 
-for img in tqdm(range(len(histo_imgs)), desc="Computing PHC"):
-    thresh_img = conditioning.threshold(histo_imgs[img])
-    prepped_image = conditioning.dilate(thresh_img) 
-    localhom = PHC(persistence_type="extended", stride=32, window_size=32) 
-    data = localhom.convolve(prepped_image)
-    data = np.array(data).reshape((256,20,20))
-    PHC_data[img] = data
 
-print("Saving File")
-np.save("extended_PHC_data", PHC_data, allow_pickle=True)
+
+
 
 
 
